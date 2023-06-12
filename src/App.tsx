2ea44370048470ca3bf04f79292120559647ebec
Jonas sousa <jonassousagtr@gmail.com>
2023-06-12 06:28:44 -0300

    Feat: Criando componente Cronometro.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Form from "./components/Form";
 import List from "./components/List";
 import style from './App.module.scss'
+import Cronometro from "./components/Cronometro";
 
 function App() {
   return (
     <div className={style.AppStyle}>
       <Form />
       <List />
+      <Cronometro />
     </div>
   );
 }
